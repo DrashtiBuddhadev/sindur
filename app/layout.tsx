7530683fc1e84d7aacc2f67b7fb2019048adf2ab
DrashtiBuddhadev <buddhadevdrashti@gmail.com>
2026-07-12 15:56:06 +0530

    error resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
       </head>
-      <body className="font-sans antialiased">
+      <body className="font-sans antialiased" suppressHydrationWarning>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
